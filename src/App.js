47325e13c54278437e79405d0605ba0c2a845963
alimahatma <laluallistilo.com>
2024-07-01 09:56:23 +0800

    use CORS method to integrate backed project-django wiht frontend project setup setting.py such us INSTALLED_APPS, MIDDLEWARE, CORS_ORIGIN_WHITELIST

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, {useState}  from 'react';
+import React, {useState, useEffect}  from 'react';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState(['Movie 1','Movie 2']);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=> {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 4e10a5822145ebc55cdf800741f0479b10a16d1c'
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setMovies(resp))
+    .catch(error => console.log(error))
+  }, [])
 
   return (
     <div className="App">
@@ -14,7 +27,7 @@ function App() {
           <div>
             {
               movies.map(movie => {
-                return <h2>{ movie }</h2>
+                return <h5>{ movie.title }</h5>
               })}
           </div>
 
